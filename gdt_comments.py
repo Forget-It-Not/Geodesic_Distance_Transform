0fb2641eab9aef2992dc8f37d6ee3df4cb462c7f
LuminAbyssus <62012024+LuminAbyssus@users.noreply.github.com>
2020-03-27 16:10:44 +0100

    Coordinates swapped
    Coordinates were swapped in the program from being described as (z,y,x) to being described as (x,y,z)
    
    HOWEVER: behavior stays the same, the change only affects to internal naming of coordinates

--- gdt_comments.py
+++ gdt_comments.py
@@ -1,13 +1,13 @@
 '''
 Module - gdt	version: 5.0
 -----------------------------
-Module for computation of chamfer distance transform of an image from a starting point
+Module for computation of chamfer distance transform of an array from a starting point (origin)
 Default method considers periodic boundaries
 
-Use: gdt.dist(array_input, seed, dist_type)
+Use: dist(array_input, origin, dist_type)
 Where:
-	- array_input: numpy binary array representing image (1 - foreground; 0 - background)
-	- seed: tuple ((z), y, x) representing coordinates used as starting point for transformation
+	- array_input: numpy binary array (1 - foreground; 0 - background)
+	- origin: tuple (x, y, (z))) representing coordinates used as starting point for transformation
 	- dist_type: distance metric used
 		"city" = cityblock
 		"chess" = chessboard
@@ -19,8 +19,8 @@ Output: array where values represent distance (may be infinite)
 ----------------------------
 
 Other functions from this module (see corresponding docstrings):
-	gdt.form3d(): quick import of 3D images into required format
-	gdt.optim(): used for restricting computation to the minimum region containing a list of points
+	form3d(): quick import of 3D images into required format
+	optim(): used for restricting computation to the minimum region containing a list of points
 
 For more information see docstrings for each function
 
@@ -30,14 +30,14 @@ import numpy as np
 from PIL import Image
 
 
-def dist (array_input, seed, dist_type, periodic_boundaries = True):
+def dist (array_input, origin, dist_type, periodic_boundaries = True):
 	'''
-	Returns distance transform of array_input using seed as starting point and using dist_type distance metric
+	Returns distance transform of array_input using origin as starting point and using dist_type distance metric
 	Periodic boundaries considered by default
 
 	Input:
-		- array_input: numpy binary array representing image (1 - foreground; 0 - background)
-		- seed: tuple ((z), y, x) representing coordinates used as starting point for transformation")
+		- array_input: numpy binary array (1 - foreground; 0 - background)
+		- origin: tuple (x, y, (z)) representing coordinates used as starting point for transformation")
 		- dist_type: distance metric used:
 			- "city" = cityblock
 			- "chess" = chessboard
@@ -46,9 +46,9 @@ def dist (array_input, seed, dist_type, periodic_boundaries = True):
 	Output: array where values represent distance (may be infinite)
 	'''
 	if array_input.ndim == 2:
-		return dist2d(array_input, seed, dist_type, periodic_boundaries)
+		return _dist2d(array_input, origin, dist_type, periodic_boundaries)
 	elif array_input.ndim == 3:
-		return dist3d(array_input, seed, dist_type, periodic_boundaries)
+		return _dist3d(array_input, origin, dist_type, periodic_boundaries)
 	else:
 		raise Exception ("Error: Incorrect array dimensions (only 2D or 3D)")
 
@@ -56,35 +56,35 @@ def dist (array_input, seed, dist_type, periodic_boundaries = True):
 
 
 
-def dist2d (array_input, seed, dist_type, periodic_boundaries):
+def _dist2d (array_input, origin, dist_type, periodic_boundaries):
 	'''
 	gdt works with an array where values:
 		-1: unactualized foreground
 		-2: background
-	A seed must be foreground and only foreground actualizable values (-1) will be modified
-	Once a value is modified with its corresponding distance > -1 its value won't change
+	An origin must be foreground, and only foreground actualizable values (-1) will be modified
+	Once a value is modified with its corresponding distance (which is > -1) its value won't change
 	'''
 	array = np.where(array_input > 0, -1, -2)
-	if array[seed] != -1:
-		raise Exception ("Error: Chosen seed is not part of foreground")
+	if array[origin] != -1:
+		raise Exception ("Error: Chosen origin is not part of foreground")
 	else:
-		array[seed] = 0
-		seed += (0,)
-		lista_fuentes = [seed]
+		array[origin] = 0
+		origin += (0,)
+		lista_fuentes = [origin]
 	'''
-	Fuente: (y, x, value) element of the array whose distance is expanded to adyacent elements in an iterative process
+	Fuente: (x, y, value) element of the array whose distance is expanded to adyacent elements in an iterative process
 	Fuentes are stored in a list and iterations stop once list is empty
 	'''
 	while lista_fuentes != []:
 		next_lista_fuentes = []
 		if dist_type in ["borges", "quasi"]:
-			order(lista_fuentes, 2)	#These dist_types require sorting the list according to their distance value
+			_order(lista_fuentes, 2)	#These dist_types require sorting the list according to their distance value
 
 		for fuente in lista_fuentes:
 			if dist_type == "city":
-				lista_adyacentes = ady2d(fuente, array)
+				lista_adyacentes = _ady2d(fuente, array)
 			else:
-				lista_adyacentes = ady2d(fuente, array, ady=8)
+				lista_adyacentes = _ady2d(fuente, array, ady=8)
 			#Only cityblock uses 4-adyacent expansion, all other methods use 8-adyacent
 
 			for index, casilla in enumerate(lista_adyacentes):
@@ -111,25 +111,25 @@ def dist2d (array_input, seed, dist_type, periodic_boundaries):
 
 
 
-def ady2d (position, array, ady=4):
+def _ady2d (position, array, ady=4):
 	'''
 	Returns list of adyacent positions to input position
 		- ady: number of adyacency (4 or 8 adyacency)
 	'''
-	SY, SX = array.shape
-	y, x, value = position
+	SX, SY = array.shape
+	x, y, value = position
 	lista_adyacentes = []
-	lista_adyacentes.append(((y-1)%SY,(x)%SX))
-	lista_adyacentes.append(((y+1)%SY,(x)%SX))
-	lista_adyacentes.append(((y)%SY,(x-1)%SX))
-	lista_adyacentes.append(((y)%SY,(x+1)%SX))
+	lista_adyacentes.append(((x-1)%SX,(y)%SY))
+	lista_adyacentes.append(((x+1)%SX,(y)%SY))
+	lista_adyacentes.append(((x)%SX,(y-1)%SY))
+	lista_adyacentes.append(((x)%SX,(y+1)%SY))
 	#First 4 values added correspond to 4-adyacents
 
 	if ady == 8:
-		lista_adyacentes.append(((y-1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((y-1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((y+1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((y+1)%SY,(x+1)%SX))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY))
+		lista_adyacentes.append(((x-1)%SX,(y+1)%SY))
+		lista_adyacentes.append(((x+1)%SX,(y-1)%SY))
+		lista_adyacentes.append(((x+1)%SX,(y+1)%SY))
 		#Second 4 values added correspond to the extra 4 found in 8-adyacents
 
 	return lista_adyacentes
@@ -144,29 +144,29 @@ def optim2d (array_input, lista_posiciones):
 	Also modifies input lista_posiciones to contain corresponding coordinates for new array
 
 	Input:
-		- array_input: numpy int array representing image. Value 1 = foreground; 0 = background
-		- lista_coordenadas: list containing lists of (y,x) coordinates
+		- array_input: numpy binary array (1 - foreground; 0 - background)
+		- lista_coordenadas: list containing lists of (x,y) coordinates
 	'''
-	ymax = max([posicion[0] for posicion in lista_posiciones])
-	ymin = min([posicion[0] for posicion in lista_posiciones])
-	xmax = max([posicion[1] for posicion in lista_posiciones])
-	xmin = min([posicion[1] for posicion in lista_posiciones])
+	xmax = max([posicion[0] for posicion in lista_posiciones])
+	xmin = min([posicion[0] for posicion in lista_posiciones])
+	ymax = max([posicion[1] for posicion in lista_posiciones])
+	ymin = min([posicion[1] for posicion in lista_posiciones])
 	#maximum and minimum values for each coordinate
 
 	nueva_lista_posiciones = []
 	for posicion in lista_posiciones:
-		nueva_lista_posiciones.append((posicion[0]-ymin, posicion[1]-xmin))
+		nueva_lista_posiciones.append((posicion[0]-xmin, posicion[1]-ymin))
 		#new coordinates in the reduced array correspond to previous coordinates - its minimum value
 	lista_posiciones[:] = nueva_lista_posiciones
 	#input list is modified in place -> replaced by new list
 
-	return array_input[ymin:ymax+1, xmin:xmax+1]
+	return array_input[xmin:xmax+1, ymin:ymax+1]
 
 #end optim2d
 
 
 
-def order (lista,dim):
+def _order (lista,dim):
 	'''
 	Function that orders a list by its dimth element
 	'''
@@ -177,27 +177,27 @@ def order (lista,dim):
 
 
 
-def dist3d (array_input, seed, dist_type, periodic_boundaries):
+def _dist3d (array_input, origin, dist_type, periodic_boundaries):
 
 	#See comments for 2D method, most code is analogous
 	array = np.where(array_input > 0, -1, -2)
-	if array[seed] != -1:
-		raise Exception ("Error: Chosen seed is not part of foreground")
+	if array[origin] != -1:
+		raise Exception ("Error: Chosen origin is not part of foreground")
 	else:
-		array[seed] = 0
-		seed += (0,)
-		lista_fuentes = [seed]
+		array[origin] = 0
+		origin += (0,)
+		lista_fuentes = [origin]
 
 	while lista_fuentes != []:
 		next_lista_fuentes = []
 		if dist_type in ["borges", "quasi"]:
-			order(lista_fuentes, 3)
+			_order(lista_fuentes, 3)
 
 		for fuente in lista_fuentes:
 			if dist_type in ["chess", "borges", "quasi"]:
-				lista_adyacentes = ady3d(fuente, array, ady=26)
+				lista_adyacentes = _ady3d(fuente, array, ady=26)
 			else:
-				lista_adyacentes = ady3d(fuente, array)
+				lista_adyacentes = _ady3d(fuente, array)
 
 			for index, casilla in enumerate(lista_adyacentes):
 				if array[casilla] == -1:
@@ -243,51 +243,51 @@ def dist3d (array_input, seed, dist_type, periodic_boundaries):
 
 
 
-def ady3d (position, array, ady=6):
+def _ady3d (position, array, ady=6):
 	'''
 	Returns list of adyacent positions to input position
 		- ady: number of adyacency (6 or 26 adyacency)
 	'''
-	SZ, SY, SX = array.shape
-	z, y, x, value = position
+	SX, SY, SZ = array.shape
+	x, y, z, value = position
 	lista_adyacentes = []
-	lista_adyacentes.append(((z-1)%SZ,(y)%SY,(x)%SX))
-	lista_adyacentes.append(((z+1)%SZ,(y)%SY,(x)%SX))
-	lista_adyacentes.append(((z)%SZ,(y-1)%SY,(x)%SX))
-	lista_adyacentes.append(((z)%SZ,(y+1)%SY,(x)%SX))
-	lista_adyacentes.append(((z)%SZ,(y)%SY,(x-1)%SX))
-	lista_adyacentes.append(((z)%SZ,(y)%SY,(x+1)%SX))
+	lista_adyacentes.append(((x-1)%SX,(y)%SY,(z)%SZ))
+	lista_adyacentes.append(((x+1)%SX,(y)%SY,(z)%SZ))
+	lista_adyacentes.append(((x)%SX,(y-1)%SY,(z)%SZ))
+	lista_adyacentes.append(((x)%SX,(y+1)%SY,(z)%SZ))
+	lista_adyacentes.append(((x)%SX,(y)%SY,(z-1)%SZ))
+	lista_adyacentes.append(((x)%SX,(y)%SY,(z+1)%SZ))
 	#1st-6th elements: single-displacement
 
 	if ady == 26:
 
-		lista_adyacentes.append(((z-1)%SZ,(y-1)%SY,(x)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y+1)%SY,(x)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y-1)%SY,(x)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y+1)%SY,(x)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z)%SZ,(y-1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z)%SZ,(y-1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z)%SZ,(y+1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z)%SZ,(y+1)%SY,(x+1)%SX))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY,(z)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y+1)%SY,(z)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y-1)%SY,(z)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y+1)%SY,(z)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x)%SX,(y-1)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x)%SX,(y-1)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x)%SX,(y+1)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x)%SX,(y+1)%SY,(z+1)%SZ))
 		#6th-18th elements: double-displacement
 
-		lista_adyacentes.append(((z-1)%SZ,(y-1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y-1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y-1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z+1)%SZ,(y-1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y+1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y+1)%SY,(x-1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y-1)%SY,(x+1)%SX))
-		lista_adyacentes.append(((z-1)%SZ,(y-1)%SY,(x-1)%SX))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y-1)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x+1)%SX,(y-1)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y+1)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y+1)%SY,(z-1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY,(z+1)%SZ))
+		lista_adyacentes.append(((x-1)%SX,(y-1)%SY,(z-1)%SZ))
 		#18th-26th elements: triple-displacement
 
 	return lista_adyacentes
 
-#end ady2d()
+#end ady3d()
 
 
 
@@ -297,25 +297,25 @@ def optim3d (array_input, lista_posiciones):
 	Also modifies input lista_posiciones to contain corresponding coordinates for new array
 
 	Input:
-		- array_input: numpy int array representing image. Value 1 = foreground; 0 = background
-		- lista_coordenadas: list containing lists of (z,y,x) coordinates
+		- array_input: numpy int array with values 1 = foreground; 0 = background
+		- lista_coordenadas: list containing lists of (x,y,z) coordinates
 	'''
 	#See coments for optim2d(), analogous code
-	zmax = max([posicion[0] for posicion in lista_posiciones])
-	zmin = min([posicion[0] for posicion in lista_posiciones])
+	xmax = max([posicion[0] for posicion in lista_posiciones])
+	xmin = min([posicion[0] for posicion in lista_posiciones])
 	ymax = max([posicion[1] for posicion in lista_posiciones])
 	ymin = min([posicion[1] for posicion in lista_posiciones])
-	xmax = max([posicion[2] for posicion in lista_posiciones])
-	xmin = min([posicion[2] for posicion in lista_posiciones])
+	zmax = max([posicion[2] for posicion in lista_posiciones])
+	zmin = min([posicion[2] for posicion in lista_posiciones])
 
 	nueva_lista_posiciones = []
 	for posicion in lista_posiciones:
-		nueva_lista_posiciones.append((posicion[0]-zmin, posicion[1]-ymin, posicion[2]-xmin))
+		nueva_lista_posiciones.append((posicion[0]-xmin, posicion[1]-ymin, posicion[2]-zmin))
 	lista_posiciones[:] = nueva_lista_posiciones
 
-	return array_input[zmin:zmax+1, ymin:ymax+1, xmin:xmax+1]
+	return array_input[xmin:xmax+1, ymin:ymax+1, zmin:zmax+1]
 
-#end optim2d
+#end optim3d
 
 
 
